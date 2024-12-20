54562eacb750fc9180afc2635db49b7d98f38840
YachavarapuKarthik <karthikyacharapu@gmail.com>
2024-12-21 00:08:17 +0530

    Url is shortening now

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"crypto/sha256"
+	"encoding/hex"
+
+)
 
 func main() {
-	fmt.Println("Url shortner")
+
+	urls := make(map[string]string)
+
+	var longUrl string
+	fmt.Scan(&longUrl)
+	hash := sha256.New()
+	hash.Write([]byte(longUrl))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+
+	urls[longUrl] = hashString[:6]
+
+	fmt.Print(longUrl,":",urls[longUrl])
+
 }
